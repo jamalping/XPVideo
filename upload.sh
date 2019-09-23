6bfa441c45b2e37ef88f3b37509d5b1ca02fb662
jamalping <420436097@qq.com>
2019-09-23 16:24:12 +0800

    0.0.1

--- upload.sh
+++ upload.sh
@@ -59,4 +59,4 @@ git push origin master --tags
 #pod trunk push XPVideo.podspec --allow-warnings
 #pod repo push xyj-xyjpodspec【私库名】 XYJBaseUI.podspec【podspec文件】 --allow-warnings[允许警告]
 #pod repo update Specs && pod repo push Specs XPVideo.podspec --verbose --allow-warnings --sources='git@git.silvrr.com:iOS/Specs.git,https://github.com/CocoaPods/Specs'
- cd ~/.cocoapods/repos/Specs && git pull origin master && cd - && pod repo push Specs XPVideo.podspec --verbose --allow-warnings --use-libraries
+ cd ~/.cocoapods/repos/master/Specs && git pull origin master && cd - && pod repo push Specs XPVideo.podspec --verbose --allow-warnings --use-libraries
